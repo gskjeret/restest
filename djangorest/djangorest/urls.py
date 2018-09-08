05d27f3e4068f7f55bc7e256580677ce2021f388
Geir <gskjeret@gmail.com>
2018-09-08 21:19:28 +0200

    Added rudimentary frontpage

--- djangorest/djangorest/urls.py
+++ djangorest/djangorest/urls.py
@@ -18,5 +18,6 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^home/', include('home.urls')),
     url(r'^', include('api.urls')) # Add this line
 ]
