0b05e842b74a7b485a59350e01ea7b54d7ab8b0f
Geir <gskjeret@gmail.com>
2018-09-22 13:29:16 +0200

    Orders: details can be hidden by clicking on the same line again

--- djangorest/static/home/js/database_ordre.js
+++ djangorest/static/home/js/database_ordre.js
@@ -5,6 +5,11 @@ $(document).ready(function()
     $('#nav_ordre').addClass("active");
 });
 
+function format ( d ) {
+    var extra_info = '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">';
+    return "";
+}
+
 // Create a request variable and assign a new XMLHttpRequest object to it.
 var request = new XMLHttpRequest();
 
@@ -37,34 +42,39 @@ request.onload = function () {
     $('#ordreliste_table tbody').on('click', 'td.details-control', function () {
         var tr = $(this).closest('tr');
         var row = table.row( tr );
-        $('#ordreliste_table tbody tr.shown').removeClass('shown')
-
-        // Show details for this row
-        tr.addClass('shown');
-
-        // Asynchronously display orderlines
-        var linerequest = new XMLHttpRequest();
-        linerequest.open('GET', 'http://127.0.0.1:8000/v_ordrelinje/?order='+row.data().ordre_id, true);
-        linerequest.onload = function () {
-            // Begin accessing JSON data here
-            var JSONlinedata = JSON.parse(this.response);
-            console.log(JSONlinedata)
-            var table = $('#ordrelinjeliste_table').DataTable( {
-                destroy: true,
-                data: JSONlinedata,
-                columns: [
-                            { data: "ordrelinje_id"},
-                            { data: "ordre"},
-                            { title: "Linje", data: "linjenr"},
-                            { title: "kode", data: "produktkode"},
-                            { title: "Produkt", data: "produktnavn"},
-                            { title: "Rabatt%", data: "rabatt_pros"},
-                            { title: "Beløp", data: "belop_linje_u_mva"},
-                            { title: "Kommentar", data: "kommentar"},
-                        ]
-            });
-        } 
+        if ( row.node().classList.contains('shown'))  {
+            // This row is already shown - unshow it and hide details table
+            $('tr').removeClass();
+            console.log($('#ordrelinjeliste_table').DataTable)
+            $('#ordrelinjeliste_table').DataTable().destroy(false);
+            $('#ordrelinjeliste_table').empty();
+        } else {
+            // Remove shown attribute from all rows
+            $('#ordreliste_table tbody tr.shown').removeClass('shown');
+            // Add it to just this row
+            tr.addClass('shown');
+            // Asynchronously display orderlines
+            var linerequest = new XMLHttpRequest();
+            linerequest.open('GET', 'http://127.0.0.1:8000/v_ordrelinje/?order='+row.data().ordre_id, true);
+            linerequest.onload = function () {
+                var JSONlinedata = JSON.parse(this.response);
+                var linetable = $('#ordrelinjeliste_table').DataTable( {
+                    destroy: true,
+                    data: JSONlinedata,
+                    columns: [
+                                { data: "ordrelinje_id"},
+                                { data: "ordre"},
+                                { title: "Linje", data: "linjenr"},
+                                { title: "kode", data: "produktkode"},
+                                { title: "Produkt", data: "produktnavn"},
+                                { title: "Rabatt%", data: "rabatt_pros"},
+                                { title: "Beløp", data: "belop_linje_u_mva"},
+                                { title: "Kommentar", data: "kommentar"},
+                            ]
+                });
+            } 
         linerequest.send();
+        }
     } 
 )} 
 
